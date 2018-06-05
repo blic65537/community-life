d678b2528820553a75826a2d7bef53acee8fc599
rookyBomb <rookyBomb@DESKTOP-ALDAIPF>
2018-06-05 18:18:43 +0900

    test

--- Life-Root/Life-Web/src/main/java/com/community/life/HomeController.java
+++ Life-Root/Life-Web/src/main/java/com/community/life/HomeController.java
@@ -20,7 +20,7 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
-	 * Simply selects the home view to render by returning its name.
+	 * TEST
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
@@ -35,5 +35,5 @@ public class HomeController {
 		
 		return "home";
 	}
-	
+
 }
